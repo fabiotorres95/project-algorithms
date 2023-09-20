99148e9d377ded03069929eaa00b3c7bbfc13139
fabiotorres95 <engfabiotorres@outlook.com>
2023-09-20 16:55:58 -0300

    created function test_encrypt_message, lint corrections

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,13 +1,16 @@
 def study_schedule(permanence_period, target_time):
-	if target_time == None:
-		return None
-	
+    if target_time is None:
+        return None
 
-	result = 0
-	for student in permanence_period:
-		if type(student) is not tuple or type(student[0]) is not int or type(student[1]) is not int:
-			return None
-		if student[0] <= target_time and student[1] >= target_time:
-			result += 1
+    result = 0
+    for student in permanence_period:
+        if (
+            type(student) is not tuple
+            or type(student[0]) is not int
+            or type(student[1]) is not int
+        ):
+            return None
+        if student[0] <= target_time and student[1] >= target_time:
+            result += 1
 
-	return result
+    return result
--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,12 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from pytest import raises
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    with raises(Exception):
+        encrypt_message(message=24, key=42)
+        encrypt_message(message='ualbax', key='xablau')
+
+    assert encrypt_message(message='xablau', key=10) == 'ualbax'
+    assert encrypt_message(message='xablau', key=3) == 'bax_ual'
+    assert encrypt_message(message='xablau', key=4) == 'ua_lbax'
