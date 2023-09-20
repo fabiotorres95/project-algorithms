717ce38b75c362fa7fbdadb16c13f87bbf091d3e
fabiotorres95 <engfabiotorres@outlook.com>
2023-09-20 16:18:24 -0300

    created function study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,4 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    # initial commit
-    raise NotImplementedError
+	if target_time == None:
+		return None
+	
+
+	result = 0
+	for student in permanence_period:
+		if type(student) is not tuple or type(student[0]) is not int or type(student[1]) is not int:
+			return None
+		if student[0] <= target_time and student[1] >= target_time:
+			result += 1
+
+	return result
