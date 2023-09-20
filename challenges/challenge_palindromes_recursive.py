4a0d2fe587d89e4475a59020e5f78349ca673ba1
fabiotorres95 <engfabiotorres@outlook.com>
2023-09-20 18:17:43 -0300

    created function is_palindrome_recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,14 @@
-def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def is_palindrome_recursive(word, low_index=0, high_index=None):
+    if type(word) != str or word == '':
+        return False
+
+    if high_index is None:
+        high_index = len(word) - 1
+
+    if low_index >= high_index:
+        return True
+    elif word[low_index] == word[high_index]:
+        result = is_palindrome_recursive(word, low_index + 1, high_index - 1)
+        return result
+    else:
+        return False
