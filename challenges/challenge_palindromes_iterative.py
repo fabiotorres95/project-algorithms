44363e9e267b1b18da260d1013c7b07e538bf81f
fabiotorres95 <engfabiotorres@outlook.com>
2023-09-22 17:48:34 -0300

    created is_palindrome_iterative

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,14 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if type(word) != str or word == '':
+        return False
+
+    high_index = len(word) - 1
+    low_index = 0
+
+    while low_index < high_index:
+        if word[low_index] != word[high_index]:
+            return False
+        low_index += 1
+        high_index -= 1
+
+    return True
