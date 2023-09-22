97476b87a3fd7b23797d4a216e3acc6ffd64ff91
fabiotorres95 <engfabiotorres@outlook.com>
2023-09-22 17:43:38 -0300

    created function find_duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,24 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    check = check_list(nums)
+    if not check:
+        return False
+
+    nums.sort()
+    index = 0
+    while index < len(nums) - 1:
+        if nums[index] == nums[index + 1]:
+            return nums[index]
+        index += 1
+
+    return False
+
+
+def check_list(nums):
+    if nums is None or type(nums) is not list or len(nums) <= 1:
+        return False
+
+    for num in nums:
+        if type(num) is not int or num < 0:
+            return False
+
+    return True
